3080832c590b96d780ac397e13a25d15c7df2be8
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2016-01-04 16:53:15 -0500

    Move hello world to lua script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate hlua;
+
+use hlua::Lua;
+
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut script = String::new();
+
+    File::open("scripts/hello.lua").unwrap()
+         .read_to_string(&mut script).unwrap();
+
+    let mut lua = Lua::new();
+    lua.openlibs(); // Load standard lua libraries
+
+    lua.execute(&script).unwrap()
 }
