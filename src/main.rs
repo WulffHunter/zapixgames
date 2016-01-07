fde7d842077a015f022b80691d028076b4ec49fb
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2016-01-07 01:47:57 -0500

    Use proper indendation

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ extern crate glium;
 mod scripts;
 
 fn main() {
- use glium::{DisplayBuild, Surface};
- scripts::hello_world();
- let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
- loop{
+    use glium::{DisplayBuild, Surface};
+
+    scripts::hello_world();
+
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    loop {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
         target.finish().unwrap();
 
-        for ev in display.poll_events(){
-            match ev{
+        for ev in display.poll_events() {
+            match ev {
                 glium::glutin::Event::Closed => return,
                 _ => ()
             }
