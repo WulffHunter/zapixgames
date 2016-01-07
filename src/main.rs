7a5af8a692a0271df3b72451be8933ce25988b15
Jared Rand <JJ@Jareds-MacBook-Pro.local>
2016-01-07 01:17:46 -0500

    Adds a glium dependency and a blue-colored window generated using glium.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+#[macro_use]
 extern crate hlua;
+extern crate glium;
 
 mod scripts;
 
 fn main() {
+    use glium::{DisplayBuild, Surface};
     scripts::hello_world();
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    loop{
+    	let mut target = display.draw();
+    	target.clear_color(0.0, 0.0, 1.0, 1.0);
+    	target.finish().unwrap();
+
+    	for ev in display.poll_events(){
+    		match ev{
+    			glium::glutin::Event::Closed => return,
+    			_ => ()
+    		}
+    	}
+	}
 }
