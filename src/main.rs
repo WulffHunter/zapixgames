320df59689a546eb8827dde0de7ac1c33da64fb3
Jared Rand <JJ@Jareds-MacBook-Pro.local>
2016-01-07 01:39:44 -0500

    Fixed spacing in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ fn main() {
  scripts::hello_world();
  let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
  loop{
-  let mut target = display.draw();
-  target.clear_color(0.0, 0.0, 1.0, 1.0);
-  target.finish().unwrap();
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
 
-  for ev in display.poll_events(){
-   match ev{
-    glium::glutin::Event::Closed => return,
-    _ => ()
-   }
-  }
- }
+        for ev in display.poll_events(){
+            match ev{
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
