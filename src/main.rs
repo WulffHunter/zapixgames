a6df26b6a9bea44c1144daa9fb528bc29475db31
Jared Rand <JJ@Jareds-MacBook-Pro.local>
2016-01-07 01:23:29 -0500

    Fixed spacing in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,19 @@ extern crate glium;
 mod scripts;
 
 fn main() {
-    use glium::{DisplayBuild, Surface};
-    scripts::hello_world();
-    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
-    loop{
-    	let mut target = display.draw();
-    	target.clear_color(0.0, 0.0, 1.0, 1.0);
-    	target.finish().unwrap();
+ use glium::{DisplayBuild, Surface};
+ scripts::hello_world();
+ let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+ loop{
+  let mut target = display.draw();
+  target.clear_color(0.0, 0.0, 1.0, 1.0);
+  target.finish().unwrap();
 
-    	for ev in display.poll_events(){
-    		match ev{
-    			glium::glutin::Event::Closed => return,
-    			_ => ()
-    		}
-    	}
-	}
+  for ev in display.poll_events(){
+   match ev{
+    glium::glutin::Event::Closed => return,
+    _ => ()
+   }
+  }
+ }
 }
