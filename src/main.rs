07072bb41e55adb52914d384522da0e2324abb70
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2016-01-13 15:45:51 -0500

    Slight cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 extern crate glium;
 extern crate hlua;
 
+use glium::{DisplayBuild, Surface, VertexBuffer, Program};
+use glium::glutin::WindowBuilder;
+use glium::index::NoIndices;
+
 mod scripts;
 
 fn main() {
-    use glium::{DisplayBuild, Surface};
-
     scripts::hello_world();
-    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+    let display = WindowBuilder::new().build_glium().unwrap();
 
     #[derive(Copy, Clone)]
     struct Vertex {
@@ -17,20 +20,21 @@ fn main() {
 
     implement_vertex!(Vertex, position);
 
-    let vertex1 = Vertex { position: [-0.5, -0.5 ] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5 ] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let shape = vec![
+        Vertex { position: [-0.5, -0.5 ] },
+        Vertex { position: [ 0.0,  0.5 ] },
+        Vertex { position: [ 0.5, -0.25] }
+    ];
 
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
+    let indices = NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let vertex_shader_src = r#"
         #version 140
 
         in vec2 position;
 
-        void main(){
+        void main() {
             gl_Position = vec4(position, 0.0, 1.0);
         }
 
@@ -41,12 +45,12 @@ fn main() {
 
         out vec4 color;
 
-        void main(){
+        void main() {
             color = vec4(1.0, 0.0, 0.0, 1.0);
         }
     "#;
 
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let program = Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
     loop {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
